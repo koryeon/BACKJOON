ded836157764a24fc6d07de0b26efbdca9606d86
koryeon <86825236+koryeon@users.noreply.github.com>
2021-12-15 23:17:35 +0900

    Initial commit

---
+++ backjoon1000.java
@@ -0,0 +1,14 @@
+package github.backjoon;
+
+import java.util.Scanner;
+
+public class backjoon1000 {
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.println(a + b);
+    }
+}

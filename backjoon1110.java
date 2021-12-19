5df1bbd14113437dabe6359dea16a6eb5a302ecf
koryeon <86825236+koryeon@users.noreply.github.com>
2021-12-20 02:39:50 +0900

    Create backjoon1110.java

---
+++ backjoon1110.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    int sum=0;
+    int a=0;
+    int b=0;
+    int N=s.nextInt();
+    int num=N;
+    int temp=0;
+        a= 10*(num%10)+((num/10)+(num%10))%10;  
+    do{
+        
+    
+        b=10*(a%10)+(a/10+a%10)%10;
+        sum+=1;
+          temp=a;
+          a=b;
+          b=temp;
+        
+        if(N==b){
+        break;
+    }
+    } while(true);
+   
+        System.out.print(sum);
+   
+   
+}
+}

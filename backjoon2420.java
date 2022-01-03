5a7e876ae0efc30cad1b6c730e9ecb51279c92dc
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-03 23:55:35 +0900

    Create backjoon2420.java

---
+++ backjoon2420.java
@@ -0,0 +1,18 @@
+package github.BACKJOON;
+import java.util.Scanner;
+public class backjoon2420 {
+
+    public static void main(String args[]){
+     Scanner sc =new Scanner(System.in);
+     long N=sc.nextInt();
+     long M=sc.nextInt();
+     long result=N-M;
+     
+     if(result<0){
+         result=result*(-1);
+     }
+     System.out.println(result);
+    }
+}
+    
+

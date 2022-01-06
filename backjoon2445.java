c7087efc385cf613330cb15294066aaaa037bd11
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-06 23:49:29 +0900

    Create backjoon2445.java

---
+++ backjoon2445.java
@@ -0,0 +1,40 @@
+package github.BACKJOON;
+import java.util.Scanner;
+public class backjoon2445 {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        
+       for(int i=1;i<N;i++){
+          for(int j=1;j<=i;j++){
+           System.out.print("*");
+       }
+         for(int a=2*N-2*i;a>0;a--){
+               System.out.print(" ");
+           }
+       
+       for(int j=1;j<=i;j++){
+              
+           System.out.print("*");
+       }
+       System.out.print("\n");
+       }
+       
+       
+       for(int i=N;i>0;i--){
+             for(int j=i;j>0;j--){
+             System.out.print("*");
+             }
+             for(int h=0;h<2*(N-i);h++){
+                 System.out.print(" ");
+             }
+             for(int j=i;j>0;j--){
+             System.out.print("*");
+             } 
+   
+             
+             System.out.print("\n");
+         }
+     
+   }
+}

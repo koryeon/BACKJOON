cfdf1644d422142faf8035a536b0804fa2e2221c
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-08 23:50:26 +0900

    Create backjoon2523.java

---
+++ backjoon2523.java
@@ -0,0 +1,19 @@
+package github.BACKJOON;
+import java.util.Scanner;
+public class backjoon2523 {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+     for(int i=1;i<=N;i++){
+          for(int j=1;j<=i;j++){
+           System.out.print("*");
+       }
+       System.out.print("\n");
+       }
+       for(int a=N-1;a>0;a--){
+             for(int b=a;b>0;b--){
+             System.out.print("*");
+             }
+             System.out.print("\n");
+         }}
+}

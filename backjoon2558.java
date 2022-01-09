3438b04cbcf5c04d7bd7e4dcfd064570b5a49532
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-09 23:54:16 +0900

    Create backjoon2558.java

---
+++ backjoon2558.java
@@ -0,0 +1,14 @@
+package github.BACKJOON;
+import java.util.Scanner;
+public class backjoon2558 {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        System.out.println(A+B);
+         
+   
+       }
+    
+}

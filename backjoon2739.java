5e5116d87d55651cd749effc2216b1b69d76cb2b
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-11 00:07:11 +0900

    Create backjoon2739.java

---
+++ backjoon2739.java
@@ -0,0 +1,14 @@
+package github.BACKJOON;
+import java.util.Scanner;
+public class backjoon2739 {
+    public static void main(String args[]){
+     
+    
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+       for(int i=1; i<10;i++){
+           System.out.println(a+" * "+i+" = "+a*i);
+       }
+    }
+    
+}

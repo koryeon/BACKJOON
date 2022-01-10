803c7493a43129fea339b03b29e361d7c8f60ee3
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-10 23:36:27 +0900

    Create backjoon2576.java

---
+++ backjoon2576.java
@@ -0,0 +1,26 @@
+package github.BACKJOON;
+import java.util.Scanner;
+public class backjoon2576 {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int sum=0;
+        int min=100;
+        int c=0;
+        for(int i=0;i<7;i++){
+           int a=s.nextInt();
+           if(a%2==1){
+           sum+=a;
+               if(min>a){
+                   min=a;
+               }
+           }else{
+               c+=1;
+           }
+        }
+    if(c==7){
+               System.out.println("-1");
+           }else{
+           System.out.println(sum);
+           System.out.println(min);}
+       }
+}

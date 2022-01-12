b690e5c17d2fcdaf7252f30cf63fb192888888c0
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-12 21:30:26 +0900

    Create backjoon2742.java

---
+++ backjoon2742.java
@@ -0,0 +1,12 @@
+package github.BACKJOON;
+import java.util.Scanner;
+public class backjoon2742 {
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        for(int i=N;i>0;i--){
+           
+            System.out.println(i);
+        }
+    }
+}

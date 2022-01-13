12a221f8714a043cdf7c6c8827ed47d79f82041c
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-13 21:23:13 +0900

    Update backjoon1008.java

--- backjoon1008.java
+++ backjoon1008.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class backjoon1008 {
     public static void main(String args[]) {
       Scanner s = new Scanner(System.in);
       int a=s.nextInt();

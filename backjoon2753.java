7994506fdc14cd0d530145c9295641e8bc421086
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-14 18:45:07 +0900

    Create backjoon2753.java

---
+++ backjoon2753.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class backjoon2753 {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int A=s.nextInt();
+       if((A%4==0 && A%100!=0 )|| A%400==0){
+           System.out.println("1");
+       }
+       else{
+           System.out.println("0");
+       }
+      }
+    
+}

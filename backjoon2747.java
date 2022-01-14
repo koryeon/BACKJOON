ad3227be3aad6d86fb90fa300ba4f273a009df25
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-14 18:45:05 +0900

    Create backjoon2747.java

---
+++ backjoon2747.java
@@ -0,0 +1,18 @@
+
+
+package github.BACKJOON;
+import java.util.Scanner;
+public class backjoon2747 {
+ public static void main(String args[]) {
+    Scanner s=new Scanner(System.in);
+    int T=s.nextInt();
+    int[] arr=new int[46];
+    int sum=0;
+    arr[0]=0;
+    arr[1]=1;
+    for(int i =2;i<=T;i++){
+        arr[i]=+arr[i-2]+arr[i-1];
+    }
+    System.out.println(arr[T]);
+   }
+}

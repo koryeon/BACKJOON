0d83cfd506ced9c3cff4f509c7c2af93a2163640
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-15 23:46:33 +0900

    Create backjoon2839.java

---
+++ backjoon2839.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class backjoon2893 {
+    public static void main(String args[]) {
+        Scanner s =new Scanner(System.in);
+        
+        int a=s.nextInt();
+        int cnt=0;
+       
+   
+       while(a>0){
+           if(a%5==0){
+               cnt+=a/5;
+               a=0; break;
+           }
+           else{
+               a-=3;
+               cnt++;
+           }
+       }
+       System.out.print((a<0)?-1:cnt);
+   
+      
+       }
+}

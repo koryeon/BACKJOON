6552fee4470be89ceb3c28e7cc3af25272e478ff
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-26 21:50:59 +0900

    Create bakcjoon4101.java

---
+++ bakcjoon4101.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class bakcjoon4101{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+       do{
+        int A=s.nextInt();
+        int B=s.nextInt();
+        if(A==0&B==0){
+            break;
+        }
+       if(A>B){
+           System.out.println("Yes");
+       }else{
+           System.out.println("No");
+       }
+       
+       }while(true);
+    }
+}

476fc87e37dbc5d2be1d195d1f2df2cdc67d7e3a
koryeon <86825236+koryeon@users.noreply.github.com>
2022-01-30 00:08:11 +0900

    Update backjoon2839.java

--- backjoon2839.java
+++ backjoon2839.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class backjoon2893 {
+public class backjoon2839 {
     public static void main(String args[]) {
         Scanner s =new Scanner(System.in);
         

0d49398cd245b767ce412933d1aaa91fffb0bc39
koryeon <86825236+koryeon@users.noreply.github.com>
2022-02-12 00:28:31 +0900

    Create backjoon10039.java

---
+++ backjoon10039.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class backjoon10036 {
+    public static void main(String args[]) {
+     Scanner s = new Scanner(System.in);
+     int sum=0;
+     for(int i=0;i<5;i++){
+         int f=s.nextInt();
+         if(f<40){
+             sum+=40;
+         }else{
+             sum+=f;
+         }
+     }
+     System.out.println(sum/5);
+     
+    }
+}

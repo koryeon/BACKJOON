19209f33102be85d0695d0b6fb67a091cd66e071
koryeon <86825236+koryeon@users.noreply.github.com>
2022-02-11 23:06:12 +0900

    Create backjoon9498.java

---
+++ backjoon9498.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class backjoon9498 {
+    public static void main(String args[]) {
+      Scanner s = new Scanner(System.in);
+      int A=s.nextInt();
+     if(A>=90){
+         System.out.println("A");
+     }
+     else if(A>=80){
+         System.out.println("B");
+     }
+     else if(A>=70){
+         System.out.println("C");
+     }
+     else if(A>=60){
+         System.out.println("D");
+     }
+     else{
+         System.out.println("F");
+     }
+    }
+}
+

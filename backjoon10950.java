f3333347eb7a2ee31f69aad4e6165f79d05ec546
koryeon <86825236+koryeon@users.noreply.github.com>
2022-02-19 23:10:51 +0900

    Create backjoon10950.java

---
+++ backjoon10950.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class backjoon10950 {
+        public static void main(String args[]){
+    
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int A=s.nextInt();
+            int B=s.nextInt();
+            System.out.println((A+B));
+             
+        }
+       
+    }
+        
+    }    

b0e702dbdb8ef4c5b2a553bd3eb8363d91ad9f96
koryeon <86825236+koryeon@users.noreply.github.com>
2022-02-24 15:28:36 +0900

    Create backjoon10992.java

---
+++ backjoon10992.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class backjoon10992 {
+        public static void main(String args[]) {
+          Scanner s = new Scanner(System.in);
+          int N=s.nextInt();
+          
+          
+          for(int i=N;i>0;i--){
+              if(N==1){
+              System.out.print("*");
+              break;
+          }
+              for(int j=i;j>1;j--){
+                  System.out.print(" ");
+              }
+                 System.out.print("*");
+             
+      
+              for(int h=2*N-2*i-1;h>0;h--){
+                
+                  if(i==1){
+                      System.out.print("*");
+                     
+                  }else System.out.print(" ");
+                    
+              }
+                if(i<N){
+                     System.out.print("*");
+                }
+                 
+              
+              System.out.print("\n");
+          }
+          
+        
+        }
+    }
